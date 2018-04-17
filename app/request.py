5de7af3f48e0b0fe4ac6dd46d5b7891a3972f2f3
Raymond Okwenda <raymondokwenda@Raymonds-MacBook-Pro.local>
2018-04-18 00:16:37 +0300

    add process results

--- app/request.py
+++ app/request.py
@@ -20,9 +20,29 @@ def get_sources(sources):
         get_sources_data = url.read()
         get_sources_response = json.loads(get_sources_data)
         sources_results = None
-        
+
         if get_sources_response['sources']:
             sources_results_list = get_sources_response['sources']
             sources_results = process_results(sources_results_list)
 
+    return sources_results
+
+def process_results(sources_list):
+    '''
+    Fuction that process the results list and transform them into a list of objects
+    Args:
+        sources_list: A list of dictionariesthat contains news sources details
+    returns:
+        sources_list: a list of news sources objects
+    '''
+
+    sources_results = []
+    for source_item in sources_list:
+        id = source_item.get('id')
+        name = source_item.get('name')
+        description = source_item.get('description')
+        url = source_item.get('url')
+        category = source_item.get('category')
+        source_object = Sources(id, name, description, url, category)
+        sources_results.append(source_object)
     return sources_results
