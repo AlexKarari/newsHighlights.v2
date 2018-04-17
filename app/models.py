888516734000b8a53daecc71578907e00c05e96d
alexkarari <k.alexander@gmail.com>
2018-04-17 16:42:12 +0300

    models.py
    content added

---
+++ app/models.py
@@ -0,0 +1,25 @@
+class Sources:
+    '''
+    Sources class to define News_sources Objects
+    '''
+
+    def __init__(self, source_id, source_name, description, url):
+        self.source_id = source_id
+        source_name = source_name
+        self.description = description
+        self.url = url
+
+
+class Articles:
+    '''
+    Articles class to define News_articles Objects
+    '''
+
+    def __init__(self, source_id, author, title, description, url, urlToImage, url, publishedAt):
+        self.source_id = source_id
+        self.author = author
+        self.title = title
+        self.description = description
+        self.url = url
+        self.urlToImage = urlToImage
+        self.publishedAt = publishedAt
