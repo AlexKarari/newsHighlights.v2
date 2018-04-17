2c5e58cbaf3ea743acf738e1ab8ab0ab8c120d1d
DavidNganga <ngashiedavid@gmail.com>
2018-04-17 17:18:02 +0300

    modified config.py

--- config.py
+++ config.py
@@ -4,6 +4,13 @@ import os
 class Config:
     pass
 
+    NEWS_ARTICLE_URL ='https://newsapi.org/v2/top-headlines?sources={}&apiKey={}''
+    NEWS_SOURCES_URL ='https://newsapi.org/v2/sources?language=en&country={}&category={}&apiKey={}'
+    NEWS_API_KEY = os.environ.get('NEWS_API_KEY')
+    SECRET_KEY = os.environ.get('SECRET_KEY')
+
+    # 'https://newsapi.org/v1/sources?apiKey='
+
 
 class ProdConfig(Config):
 
--- tests/test_sources.py
+++ tests/test_sources.py
@@ -1,20 +1,22 @@
-# import unittest
-# # from app.models import Newssource
-# # Newssource = Newssource.Newssource
+ import unittest
+ from app.models import News_source
+ 
 
-# class NewssourceTest(unittest.TestCase):
-#     '''
-#     Test Class to test the behaviour of the Newssource class
-#     '''
+ class News_sources_Test(unittest.TestCase):
+     '''
+     Test Class to test the behaviour of the Newssource class
 
-#     def setUp(self):
-#         '''
-#         Set up method that will run before every Test
-#         '''
-#         self.new_Newssource = Newssource(1234,'ABC.com','Tom')
+     '''
 
-#     def test_instance(self):
-#         self.assertTrue(isinstance(self.new_Newssource,Newssource))
+     def setUp(self):
+         '''
+         Set up method that will run before every Test
+
+         '''
+         self.new_source = News_source(1234,'ABC.com','Tom')
+
+     def test_instance(self):
+         self.assertTrue(isinstance(self.new_source,News_sources))
 
 
 
