13ace9965aa34d900d8591162c47a9f7db21acde
Raymond Okwenda <raymondokwenda@Raymonds-MacBook-Pro.local>
2018-04-17 17:14:49 +0300

    add get_sources

--- app/request.py
+++ app/request.py
@@ -11,3 +11,18 @@ def configure_request(app):
     global api_key, base_url
     api_key = app.config['NEWS_API_KEY']
     base_url = app.config['NEWS_API_BASE_URL']
+def get_sources(sources):
+    '''
+    Fuction to retrieve news sources list from the News api_key
+    '''
+    get_sources_url = base_url.format(sources, api_key)
+    with urllib.request.urlopen(get_sources_url) as url:
+        get_sources_data = url.read()
+        get_sources_response = json.loads(get_sources_data)
+        sources_results = None
+        
+        if get_sources_response['sources']:
+            sources_results_list = get_sources_response['sources']
+            sources_results = process_results(sources_results_list)
+
+    return sources_results
