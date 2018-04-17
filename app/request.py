e47b2cce75db19f9584ba3ea72bda8a537581e2b
Raymond Okwenda <raymondokwenda@Raymonds-MacBook-Pro.local>
2018-04-17 16:32:25 +0300

    add configure_request

---
+++ app/request.py
@@ -0,0 +1,13 @@
+import urllib.request
+import json
+from .models import sources, Articles
+
+# Getting the API KEY
+api_key = None
+
+# Getting the news base url
+base_url = None
+def configure_request(app):
+    global api_key, base_url
+    api_key = app.config['NEWS_API_KEY']
+    base_url = app.config['NEWS_API_BASE_URL']
